dd0b470a6f5ffcf758b6244eae3f07b782d97fa0
yash9985 <94554813+yash9985@users.noreply.github.com>
2022-09-18 13:46:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,400);
+    canvas.position(560,120);
+   poseNet =  ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    background('#FF0000');
+    fill("blue");
+    textSize(difference);
+    text("Yash",100,100);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results){
+    if (results.length > 0){
+     console.log(results);
+
+     leftWristX = results[0].pose.leftWrist.x;
+     rightWristX = results[0].pose.rightWrist.x;
+     difference = floor(leftWristX - rightWristX);
+
+     console.log("leftWrist = " + leftWristX + "rightWrist = " + rightWristX + "difference = " + difference);
+    }
+}
